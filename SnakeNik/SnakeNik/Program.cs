28c9c99aca29e0941d71189a201143d159463bb6
TrueSkiminok <antonov.nikita.s@gmail.com>
2018-11-22 23:25:03 +0300

    Создали перечисление направлений и класс, реализующий змейку

--- SnakeNik/SnakeNik/Program.cs
+++ SnakeNik/SnakeNik/Program.cs
@@ -28,7 +28,8 @@ namespace SnakeNik
 
             // Отрисовка точек
             Point p = new Point(4, 5, '*');
-            p.Draw();
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
 
             Console.ReadLine();
         }
