84154770f5019ba50f8e87680bc6fea413b40d6c
TrueSkiminok <antonov.nikita.s@gmail.com>
2018-11-22 22:16:34 +0300

    Создали список

--- SnakeNik/SnakeNik/Program.cs
+++ SnakeNik/SnakeNik/Program.cs
@@ -11,10 +11,20 @@ namespace SnakeNik
         static void Main(string[] args)
         {
             Point p1 = new Point(1, 3, '*');
-            p1.Draw();
-
             Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            Point p3 = new Point(7, 0, '^');
+            Point p4 = new Point(6, 6, '&');
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+
+            foreach (var item in pList)
+            {
+                item.Draw();
+            }
 
             Console.ReadLine();
         }
